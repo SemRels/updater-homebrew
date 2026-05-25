8bec7773ba71a6eb3bcaed4074b8e8d388ca78bd
Markus Waldheim <31657644+mwaldheim@users.noreply.github.com>
2026-05-25 20:23:45 +0200

    feat: migrate implementation from semrel core (#7)
    Signed-off-by: mwaldheim <31657644+mwaldheim@users.noreply.github.com>

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -1,25 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2026 The plugin-template Authors
+// SPDX-FileCopyrightText: 2026 The semrel Authors
 
 package main
 
 import (
-	"context"
 	"log"
-	"os"
 
-	grpcserver "github.com/SemRels/plugin-template/internal/grpc"
-	semrelplugin "github.com/SemRels/plugin-template/internal/plugin"
+	plugin "github.com/SemRels/updater-homebrew/internal/plugin"
 )
 
 func main() {
-	provider := semrelplugin.NewProvider("replace-me")
-	server := grpcserver.NewProviderServer(provider)
-
-	if _, err := server.Health(context.Background()); err != nil {
-		log.Printf("plugin health check failed: %v", err)
-		os.Exit(1)
-	}
-
-	log.Printf("%s plugin template is ready", provider.Name())
+	updater := plugin.NewUpdater(plugin.FormulaConfig{})
+	log.Printf("updater-homebrew plugin ready: updates Homebrew formulae (%T)", updater)
 }
